0d14f207ca522619ebe1d5edfb5662d7f36f9c51
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-09-15 20:00:28 -0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import Profile from "./components/Profile";
+import Home from "./components/Home";
 import { useAuth0 } from '@auth0/auth0-react';
 function App() {
   const { isAuthenticated } = useAuth0();
