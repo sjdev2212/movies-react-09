209737bbb3e60b615aad6e2888bf0fa46687f83c
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-09-25 11:15:33 -0300

    add details files

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,42 +1,36 @@
-import { useEffect,useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 
-const Detalle = () => {
-   const [detalle, setDetalle] = useState([]);
-  const navigate = useNavigate()
-  const params = useParams()
-  const apiEndpoint = `https://api.themoviedb.org/3/movie/${params.id}?api_key=5e63572e6af98e257733a794be08a3f8&language=en-US`
- 
+const Details = () => {
+  const [detalle, setDetalle] = useState([]);
 
-useEffect (() => {
-  const token = sessionStorage.getItem("token")
-  if (!token) {
-    navigate("/")
-  }
-  axios.get(apiEndpoint).then(res => {
-  
-    setDetalle(res.data)
-  } )
+  const params = useParams();
+  const apiEndpoint = `https://api.themoviedb.org/3/movie/${params.id}?api_key=5e63572e6af98e257733a794be08a3f8&language=en-US`;
 
-},[navigate,setDetalle,apiEndpoint] )
-
-
+  useEffect(() => {
+    axios.get(apiEndpoint).then((res) => {
+      setDetalle(res.data);
+    });
+  }, [setDetalle, apiEndpoint]);
 
   return (
     <>
-    <div className='row'>
-    <h2>{detalle.title}</h2>
-        <div className='col-4'>
-        <img src={`https://image.tmdb.org/t/p/w500/${detalle.poster_path}`} className="card-img-top" alt="..." />
+      <div className="row">
+        <h2>{detalle.title}</h2>
+        <div className="col-4">
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${detalle.poster_path}`}
+            className="card-img-top"
+            alt="..."
+          />
         </div>
-        <div className='col-8'>
-        <p>{detalle.overview}</p>
+        <div className="col-8">
+          <p>{detalle.overview}</p>
         </div>
-      
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Detalle
+export default Details;
